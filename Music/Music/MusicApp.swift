27af503cec07d853abc30f55680f7d6f1ad20193
Ocean Chen <oche@unc.edu>
2025-02-13 14:35:33 -0500

    First Push

---
+++ Music/Music/MusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicApp.swift
+//  Music
+//
+//  Created by Ocean Chen on 2/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Music/MusicTests/MusicTests.swift
@@ -0,0 +1,17 @@
+//
+//  MusicTests.swift
+//  MusicTests
+//
+//  Created by Ocean Chen on 2/11/25.
+//
+
+import Testing
+@testable import Music
+
+struct MusicTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
